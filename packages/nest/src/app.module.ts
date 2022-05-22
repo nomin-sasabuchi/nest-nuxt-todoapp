3d3f209c547d104443b0bbca0c13633592075c29
nomin sasabuchi <nomin.sasabuchi@karabiner.tech>
2022-05-22 12:50:43 +0900

    データベース接続問題解消

--- packages/nest/src/app.module.ts
+++ packages/nest/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ItemsModule } from 'items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'auth/auth.module';
+import { ItemsModule } from 'items/items.module';
 
 @Module({
   imports: [ItemsModule, TypeOrmModule.forRoot(), AuthModule],
--- packages/nest/src/migrations/1653191027940-CreateItem.ts
+++ packages/nest/src/migrations/1653191027940-CreateItem.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateAddUserId1647859397769 implements MigrationInterface {
-    name = 'CreateAddUserId1647859397769'
+export class CreateItem1653191027940 implements MigrationInterface {
+    name = 'CreateItem1653191027940'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "password" character varying NOT NULL, "status" character varying NOT NULL, CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`, undefined);
